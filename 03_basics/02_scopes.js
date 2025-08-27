b81ae9aea442ba555410fdbc58d2574266ddb348
yatharth202 <133094803+yatharth202@users.noreply.github.com>
2025-08-27 14:02:10 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,18 +1,60 @@
 
-let a =300 
+// let a =300 
 
-if( true){
-    let a = 10;
-    const b =20;
-    var c = 30;  
-}
+// if( true){
+//     let a = 10;
+//     const b =20;
+//     var c = 30;  
+// }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
+// }
+
+// //console.log(a);
+// //console.log(b);
+// console.log(c); // print hoogya scope issue
+  
+
+function one(){
+    const username = "Yatharth"
+
+    function two(){
+        const website =  "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two();
 }
 
-//console.log(a);
-//console.log(b);
-console.log(c); // print hoogya scope issue
- 
+one();
+
+if(true){
+    const username = "Yatharth"
+    if(username== "Yatharth"){
+        const website = "youtube"
+        console.log( username + website)
+    }
+
+    // console.log(website)
+}
+
+// console.log(username);
+
+// ++++++++++++++++++++++++++++++ intresting +++++++++++++
+console.log(addone(5)) // will not give error
+
+function addone(num){
+    return num + 1;
+} // this is function 
+
+
+
+addTwo(5) // will give error 
+const addTwo =function(num){  // this is also function but also called expressions because it is holding values
+    return num+ 2;
+}
+
+// addTwo(5)

523f25c60527a6f74e935256374bbf89fa591fbd
yatharth202 <133094803+yatharth202@users.noreply.github.com>
2025-08-27 13:25:39 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,18 @@
+
+let a =300 
+
+if( true){
+    let a = 10;
+    const b =20;
+    var c = 30;  
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+//console.log(a);
+//console.log(b);
+console.log(c); // print hoogya scope issue
+ 

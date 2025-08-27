b743d10540ccdb1c47a1af43da2b41829b726c7e
yatharth202 <133094803+yatharth202@users.noreply.github.com>
2025-08-27 20:40:12 +0000

    control flow

---
+++ 04_control_flow/01_basics.js
@@ -0,0 +1,18 @@
+// if 
+
+const isUserloggedfIn = true
+if(2==="2"){
+    console.log("executed");
+}
+if(false){
+
+}
+
+// falsy Values
+
+// false , 0 , -0 , NigInt 0n , "" , null ,undefined ,NaN
+// baki all true
+
+// truthy values
+// "0" , 'false' , " " , [] , {} , funcation(){}
+
